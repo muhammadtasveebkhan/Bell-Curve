3bed1eebbe033f12b703a28e90f1863f98ad036e
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-01 16:29:53 +0000

    Apr 1, 2026, 12:29 PM

--- src/assets/sprites.py
+++ src/assets/sprites.py
@@ -16,4 +16,8 @@ class DataPoint(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(0, screen_width - 20)
         self.rect.y = -20
-        self.speed = random.ranint(2, 5)        
+        self.speed = random.ranint(2, 5)
+    
+    def update(self):
+        self.rect.y += self.speed
+                    
