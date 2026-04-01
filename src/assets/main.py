802f79acb503ba4fc4fe66c008fafe77d7ec72b2
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-01 19:38:08 +0000

    Apr 1, 2026, 3:38 PM

--- src/assets/main.py
+++ src/assets/main.py
@@ -1,26 +1,26 @@
-# import pygame
-# import os 
-# import random
-# from sprites import DataPoint
+import pygame
+import os 
+import random
+from sprites import DataPoint
 
-# # # Project Path Logic 
-# GAME_PATH = os.path.dirname(os.path.abspath(__file__))
+# # Project Path Logic 
+GAME_PATH = os.path.dirname(os.path.abspath(__file__))
 
-# def get_asset_path(filename:str) -> str:
-#     return os.path.join(GAME_PATH,"assets", filename)
+def get_asset_path(filename:str) -> str:
+    return os.path.join(GAME_PATH,"assets", filename)
 
-# #Initializing Pygame
-# pygame.init()
-# SCREEN_WIDTH = 800
-# SCREEN_HEIGHT = 600
-# screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-# pygame.display.set_caption("Bell Curve Catcher")
-# clock = pygame.time.Clock()
+#Initializing Pygame
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Bell Curve Catcher")
+clock = pygame.time.Clock()
 
-# #Colors
-# WHITE = (255, 255, 255)
-# BLACK = (0, 0, 0)
-# GRAY = (200, 200, 200)
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
 
 # def main():
 #     running = True
