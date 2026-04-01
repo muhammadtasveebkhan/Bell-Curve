5bb7fb1ca842ff4fdbfef020a6e71376a2e8342c
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-01 14:59:55 +0000

    Apr 1, 2026, 10:59 AM

--- src/assets/main.py
+++ src/assets/main.py
@@ -14,4 +14,12 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("Bell Curve Catcher")
+clock = pygame.time.Clock()
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+
 
