a5c67ff4890459bab02a68ee2bd49d9d3e33b9f1
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-01 14:41:00 +0000

    Apr 1, 2026, 10:41 AM

--- src/assets/main.py
+++ src/assets/main.py
@@ -6,3 +6,5 @@ from sprites import DataPoint
 # Project Path Logic 
 Game_Path = os.path.dirname(os.path.abspath(__file__))
 
+def get_asset_path(filename:str) -> str:
+    return os.path.join(GAME_PATH,"assets", filename)
