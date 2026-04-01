711431553f9c59ec2b290cdea2203f4e8f4fb219
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-01 19:45:40 +0000

    Apr 1, 2026, 3:45 PM

--- src/assets/msin1.py
+++ src/assets/msin1.py
@@ -1,13 +1,12 @@
-import pygame
-pygame.init()
-
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
 def main():
     # Example file showing a basic pygame "game loop"
-   
+    import pygame
+    pygame.init()
+
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
 
     while running:
         # poll for events
