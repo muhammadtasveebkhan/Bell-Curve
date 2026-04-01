2f253106a02c1959d6fd1d9e07101cb2388af174
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-01 14:41:52 +0000

    Apr 1, 2026, 10:41 AM

--- src/assets/main.py
+++ src/assets/main.py
@@ -4,7 +4,10 @@ import random
 from sprites import DataPoint
 
 # Project Path Logic 
-Game_Path = os.path.dirname(os.path.abspath(__file__))
+GAME_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def get_asset_path(filename:str) -> str:
     return os.path.join(GAME_PATH,"assets", filename)
+
+#Initializing Pygame
+
