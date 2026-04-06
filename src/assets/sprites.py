0615fe0b5fc309454c6a534e4427236505225fac
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-06 17:02:33 +0000

    Apr 6, 2026, 1:02 PM

--- src/assets/sprites.py
+++ src/assets/sprites.py
@@ -5,4 +5,10 @@ class DataPoint(pygame.sprite.Sprite):
     '''Represents the falling data points (Green = Good, Red = Outlier).'''
     def __init__(self, screen_width):
         super().__init__()
-        # Logic: 30% chance to
+        # Logic: 30% chance to be an outlier 
+        self.is_outlier = random.random() > 0.7 
+
+        self.image = pygame.Surface((20, 20))
+        if self.is_outlier:
+            self.image.fill((255, 0, 0)) # Red
+            
