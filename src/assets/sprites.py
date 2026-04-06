e6be90c227312de172a5ed013ec98219373dd9f6
Muhammad Tasveeb Khan <muhammadtasveebkhan@gmail.com>
2026-04-06 17:01:56 +0000

    Apr 6, 2026, 1:01 PM

--- src/assets/sprites.py
+++ src/assets/sprites.py
@@ -1,1 +1,8 @@
-      
+import pygame
+import random
+
+class DataPoint(pygame.sprite.Sprite):
+    '''Represents the falling data points (Green = Good, Red = Outlier).'''
+    def __init__(self, screen_width):
+        super().__init__()
+        # Logic: 30% chance to
